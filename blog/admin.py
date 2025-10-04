f4fe0b7a39afd8af5a6392b5d57dd700f42d6110
sepehr gorji <freshsgr@gmail.com>
2025-10-04 17:02:48 +0330

    general ordering

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,6 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "-empty-"
     list_display = ["title", "counted_views", "status","published_date"]
     list_filter = ("status",)
-    ordering = ["-created_date"]
+    # ordering = ["-created_date"]
     search_fields = ["title", "content"] 
 admin.site.register(Post,PostAdmin)
