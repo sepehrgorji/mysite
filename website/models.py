525add9a95063573ae9fa1858c758d5c9ad730c0
sepehr gorji <freshsgr@gmail.com>
2025-10-05 16:32:54 +0330

    contact

--- website/models.py
+++ website/models.py
@@ -4,4 +4,10 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    subject = models.CharField(max_length=255)    
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)    
+
+    def __str__(self):
+        return self.name
