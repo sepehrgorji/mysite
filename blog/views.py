33312c412f6873c0ea2f3a162553bee4309c1612
sepehr gorji <freshsgr@gmail.com>
2025-10-05 16:49:02 +0330

    create test temlplate

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 def blog_view(requset):
     return render(requset, 'blog/blog-home.html')
 
 def blog_single(requset):
     return render(requset, 'blog/blog-single.html')
+
+def test(request):
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(request, 'test.html',context)
