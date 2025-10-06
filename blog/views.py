ab249a462668b4bac48529cab0a4a125e60588d6
sepehr gorji <freshsgr@gmail.com>
2025-10-06 16:57:59 +0330

    some changes

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from blog.models import Post
 # Create your views here.
 def blog_view(requset):
-    return render(requset, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(requset, 'blog/blog-home.html',context)
 
 def blog_single(requset):
     return render(requset, 'blog/blog-single.html')
