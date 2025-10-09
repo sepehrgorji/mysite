8600a315cc55c6bedb4039e3112f6afa4511269a
sepehr gorji <freshsgr@gmail.com>
2025-10-09 19:10:41 +0330

    author,model

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 # Create your views here.
 def blog_view(requset):
@@ -6,8 +6,10 @@ def blog_view(requset):
     context = {'posts':posts}
     return render(requset, 'blog/blog-home.html',context)
 
-def blog_single(requset):
-    return render(requset, 'blog/blog-single.html')
+def blog_single(requset,pid):
+    posts = get_object_or_404(Post,pk=pid, status=1)
+    context = {'post':posts}
+    return render(requset, 'blog/blog-single.html',context)
 
 def test(request):
     posts = Post.objects.filter(status=1)
