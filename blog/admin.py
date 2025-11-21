e9220cd1f97337a77f919606841a01f718d33cd9
sepehr gorji <freshsgr@gmail.com>
2025-11-21 11:50:00 +0330

    saff

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from blog.models import Post
+
 # Register your models here.
+
 
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = "-empty-"
-    list_display = ["title", "author" ,"counted_views", "status","published_date"]
-    list_filter = ("status","author")
+    list_display = ["title", "author", "counted_views", "status", "published_date"]
+    list_filter = ("status", "author")
     # ordering = ["-created_date"]
-    search_fields = ["title", "content"] 
-admin.site.register(Post,PostAdmin)
+    search_fields = ["title", "content"]
+
+
+admin.site.register(Post, PostAdmin)
